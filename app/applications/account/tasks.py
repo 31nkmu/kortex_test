125071dd273c2ed689169e45f0f435d7f111ecbc
billal <karimovbillal20002@gmail.com>
2023-06-10 21:29:27 +0600

    Add __init__

--- app/applications/account/tasks.py
+++ app/applications/account/tasks.py
@@ -6,7 +6,8 @@ from django.core.mail import send_mail
 
 @app.task
 def send_user_activation_link(email, activation_code):
-    full_link = f'http://{os.environ.get("HOST")}:{os.environ.get("NGINX_PORT", "PORT")}/api/v1/account/activate/{activation_code}'
+    full_link = f'http://{os.environ.get("HOST")}:{os.environ.get("NGINX_PORT", "PORT")}' \
+                f'/api/v1/account/activate/{activation_code}'
     send_mail(
         'from electronic store',
         f'Your activation link {full_link}',
